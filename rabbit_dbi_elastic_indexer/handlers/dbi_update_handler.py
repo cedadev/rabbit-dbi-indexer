2c073ef956b0bdb187a19c7ef5aec0899c92bcbc
rsmith013 <richard.d.smith@stfc.ac.uk>
2021-03-26 11:30:53 +0000

    fix config lookup

--- rabbit_dbi_elastic_indexer/handlers/dbi_update_handler.py
+++ rabbit_dbi_elastic_indexer/handlers/dbi_update_handler.py
@@ -34,7 +34,7 @@ class DirectoryUpdateHandler(UpdateHandler):
         self.index_updater = CedaDirs(
             index=self.conf.get('directory_index', 'name'),
             **{'headers': {
-                'x-api-key': self.conf.get('elasticsearch', 'es-api-key')
+                'x-api-key': self.conf.get('elasticsearch', 'es_api_key')
             },
                 'retry_on_timeout': True,
                 'timeout': 30
